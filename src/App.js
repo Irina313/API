644a5bc7df4f4c527a8abf370b46b4854e3d00ce
Irina <irina1989_ru@ukr.net>
2022-09-03 00:57:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from "react";
 
-function App() {
+class App extends Component {
+state= {
+  content: null,
+  author: null
+}
+
+async componentDidMount() {
+  const url = "https://api.quotable.io/random";
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data)
+}
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
+}
 }
 
 export default App;
